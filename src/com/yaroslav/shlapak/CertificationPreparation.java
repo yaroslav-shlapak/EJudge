564fbf2d14cd350c8c0e3d059c5d0602ec2128e8
yaroslav <y.shlapak@samsung.com>
2016-03-03 19:24:31 +0200

    Correct certification preparation problem solving was added

--- src/com/yaroslav/shlapak/CertificationPreparation.java
+++ src/com/yaroslav/shlapak/CertificationPreparation.java
@@ -1,4 +1,4 @@
-package com.yaroslav.shlapak;
+//package com.yaroslav.shlapak;
 
 import java.util.Scanner;
 
@@ -15,45 +15,36 @@ public class CertificationPreparation {
             data[i] = scanner.nextInt();
         }
 
-
         //int[] data = {4, 3, 5, 1, 5, 6, 2, 7, 3, 8, 4, 2, 3, 5, 1, 7, 9, 6, 7, 7, 10, 3, 10, 8, 9, 2, 7, 10};
         //int len = data.length - 1;
 
-        int maxNum = 0;
-
-        for(int i = 0; i < len; i++) {
-            int index = i;
-            int num = 0;
-            while (index != -1) {
-                System.out.print(data[index] + " ");
-                index = getNearestMin(data, index);
-
-                num++;
-            }
-            if(num > maxNum) {
-                maxNum = num;
-            }
-            System.out.println();
-
-        }
-        System.out.println(maxNum);
+        System.out.println(longestIncreasingSubsequence(data));
     }
 
-    private static int getNearestMin(int[] data, int index) {
 
-        int indexOfMin = data.length - 1;
-        boolean latch = false;
-        for(int i = data.length - 1; i > index; i--) {
-            if(data[index] < data[i] && data[i] <= data[indexOfMin]) {
-                indexOfMin = i;
-                latch = true;
+    public static int longestIncreasingSubsequence(int[] input) {
+
+        if(input == null || input.length == 0) {
+            return 0;
+        }
+
+        int n = input.length;
+        // lisLength[i] - Length of Longest Increasing Subsequence in input[0..i]
+        int[] lisLength = new int[n];
+        int maxLength = 1;
+        lisLength[0] = 1;
+        for (int i = 1; i < n; i++) {
+            lisLength[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (input[i] > input[j] && lisLength[i] < lisLength[j] + 1) {
+                    lisLength[i] = lisLength[j] + 1;
+                }
             }
-
+            // Update the length of longest increasing subsequence found till now
+            if(maxLength < lisLength[i]) {
+                maxLength = lisLength[i];
+            }
         }
-        if(latch) {
-            return indexOfMin;
-        } else {
-            return -1;
-        }
+        return maxLength;
     }
 }
