1dc616ab17aedaf27fcb2e7bfd54a6198c10163b
y.shlapak <y.shlapak@samsung.com>
2016-03-04 17:34:08 +0200

    Tourist Routes problem was started

---
+++ src/com/yaroslav/shlapak/MountainRoutes.java
@@ -0,0 +1,20 @@
+package com.yaroslav.shlapak;
+
+import java.util.Scanner;
+
+/**
+ * Created by y.shlapak on Mar 04, 2016.
+ */
+public class MountainRoutes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n, k, a, b, d;
+        n = scanner.nextInt();
+        k = scanner.nextInt();
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        d = scanner.nextInt();
+
+    }
+}
