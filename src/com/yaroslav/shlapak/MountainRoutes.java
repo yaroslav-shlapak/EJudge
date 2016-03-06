e5409d502be5dbc4f64d04487d3c36f4ab50ceca
yaroslav <yaroslav.shlapak@gmail.com>
2016-03-07 00:23:40 +0200

    Working changes was saed

--- src/com/yaroslav/shlapak/MountainRoutes.java
+++ src/com/yaroslav/shlapak/MountainRoutes.java
@@ -1,5 +1,7 @@
 package com.yaroslav.shlapak;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -16,5 +18,46 @@ public class MountainRoutes {
         b = scanner.nextInt();
         d = scanner.nextInt();
 
+        int[][] routes = new int[n][n];
+        for(int horz, vert, i = 0; i < k; i++) {
+            horz = scanner.nextInt() - 1;
+            vert = scanner.nextInt() - 1;
+            System.out.print(horz + " " + vert + "\n");
+            routes[horz][vert] = 1;
+        }
+        for(int i = 0; i < n; i++) {
+            System.out.println(Arrays.toString(routes[i]));
+        }
+
+        Stack routesStack = new Stack(100);
+        while()
+    }
+}
+
+class Stack {
+    private int maxSize;
+    private int[] stackArray;
+    private int top;
+
+    public Stack(int s) {
+        maxSize = s;
+        stackArray = new int[maxSize];
+        top = -1;
+    }
+
+    public void push(int j) {
+        stackArray[++top] = j;
+    }
+    public long pop() {
+        return stackArray[top--];
+    }
+    public long peek() {
+        return stackArray[top];
+    }
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+    public boolean isFull() {
+        return (top == maxSize);
     }
 }
