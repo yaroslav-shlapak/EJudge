d7f00ecf04940530c5e7bd570560cb03b029b9a9
yaroslav <y.shlapak@samsung.com>
2016-03-15 12:07:57 +0200

    ShortestPath problem was solved

--- src/com/yaroslav/shlapak/ShortestPath.java
+++ src/com/yaroslav/shlapak/ShortestPath.java
@@ -1,15 +1,30 @@
 package com.yaroslav.shlapak;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * Created by yaroslav on 3/13/16.
  */
+
+/*
+10 10
+1 10
+1 2
+1 3
+1 4
+4 5
+2 6
+5 7
+6 8
+7 8
+8 10
+8 9
+ */
 public class ShortestPath {
     static final int MAX_N = 50000;
     static final int MAX_M = 100000;
+    static final int ADD = 10;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -19,9 +34,8 @@ public class ShortestPath {
         int a = scanner.nextInt(); // start
         int b = scanner.nextInt(); // finish
 
-
-        int[][] adjacencyList = new int[n + 1][n + 1];
-        int[] size = new int[n + 1];
+        /*int[][] adjacencyList = new int[n + ADD][n + ADD];
+        int[] size = new int[n + ADD];
 
         for(int i = 0; i < m; i++) {
             int horz = scanner.nextInt() - 1;
@@ -29,17 +43,34 @@ public class ShortestPath {
             adjacencyList[horz][size[horz]++] = vert + 1;
             adjacencyList[vert][size[vert]++] = horz + 1;
         }
-        breadthFirstSearch(n, a, b, adjacencyList, size);
+        breadthFirstSearch(n, a, b, adjacencyList, size);*/
+
+        ArrayList<Integer>[] adjacencyList = new ArrayList[n];
+
+        for(int i = 0; i < m; i++) {
+
+            int horz = scanner.nextInt() - 1;
+            int vert = scanner.nextInt() - 1;
+            if(adjacencyList[horz] == null) {
+                adjacencyList[horz] = new ArrayList<>();
+            }
+            if(adjacencyList[vert] == null) {
+                adjacencyList[vert] = new ArrayList<>();
+            }
+            adjacencyList[horz].add(vert + 1);
+            adjacencyList[vert].add(horz + 1);
+        }
+        breadthFirstSearch(n, a, b, adjacencyList);
     }
 
     private static void breadthFirstSearch(int n, int a, int b, int[][] adjacencyList, int[] size) {
-        int[] path = new int[n + 1];
-        int[] shortestPath = new int[n + 1];
-        int[] visited = new int[n + 1];
+        int[] path = new int[n + ADD];
+        int[] shortestPath = new int[n + ADD];
+        int[] visited = new int[n + ADD];
         int length = 0;
 
         boolean found = false;
-        Queue verticeQueue = new Queue(n + 1);
+        Queue verticeQueue = new Queue(n + ADD);
 
         verticeQueue.enqueue(a);
 
@@ -64,7 +95,7 @@ public class ShortestPath {
                 //System.out.println(length);
                 //System.out.println(Arrays.toString(shortestPath));
 
-                for(int j = 0; j < length  / 2; j++) {
+                for(int j = 0; j < length  / 2 + 1; j++) {
                     int temp = shortestPath[length - j];
                     shortestPath[length - j] = shortestPath[j];
                     shortestPath[j] = temp;
@@ -87,7 +118,68 @@ public class ShortestPath {
             System.out.println(-1);
         } else {
             System.out.println(length);
-            for(int i = 0; i < length + 1; i++) {
+            for(int i = 0; i <= length; i++) {
+                System.out.print(shortestPath[i] + " ");
+            }
+        }
+
+    }
+
+    private static void breadthFirstSearch(int n, int a, int b, ArrayList<Integer>[] adjacencyList) {
+        int[] path = new int[n + ADD];
+        int[] shortestPath = new int[n + ADD];
+        int[] visited = new int[n + ADD];
+        int length = 0;
+
+        boolean found = false;
+        Queue verticeQueue = new Queue(n + ADD);
+
+        verticeQueue.enqueue(a);
+
+        visited[a - 1] = 1;
+
+        path[0] = a;
+        path[a - 1] = a;
+        while (!verticeQueue.isEmpty()) {
+            //verticeQueue.display();
+            int currentVertice = verticeQueue.dequeue();
+
+            if(currentVertice == b) {
+                found = true;
+                int x = currentVertice;
+
+                shortestPath[length] = x;
+
+                while(x != a) {
+                    x = path[x - 1];
+                    shortestPath[++length] = x;
+                }
+                //System.out.println(length);
+                //System.out.println(Arrays.toString(shortestPath));
+
+                for(int j = 0; j < length  / 2 + 1; j++) {
+                    int temp = shortestPath[length - j];
+                    shortestPath[length - j] = shortestPath[j];
+                    shortestPath[j] = temp;
+                }
+                //System.out.println(Arrays.toString(shortestPath));
+                break;
+            }
+
+            for(int vertice : adjacencyList[currentVertice - 1]) {
+                if(visited[vertice - 1] == 0) {
+                    verticeQueue.enqueue(vertice);
+                    visited[vertice - 1] = 1;
+                    path[vertice  - 1] = currentVertice;
+                }
+            }
+        }
+
+        if(!found) {
+            System.out.println(-1);
+        } else {
+            System.out.println(length);
+            for(int i = 0; i <= length; i++) {
                 System.out.print(shortestPath[i] + " ");
             }
         }
