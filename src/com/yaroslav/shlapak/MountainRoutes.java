5e381b6352f9f7c080f4ca8aac581ecd76f34e73
yaroslav <y.shlapak@samsung.com>
2016-03-16 17:44:29 +0200

    Moutain Routes solved using recursion

--- src/com/yaroslav/shlapak/MountainRoutes.java
+++ src/com/yaroslav/shlapak/MountainRoutes.java
@@ -8,6 +8,10 @@ import java.util.Scanner;
  * Created by y.shlapak on Mar 04, 2016.
  */
 public class MountainRoutes {
+    static int counter = 0;
+    static int numberOfDays = 0;
+    static int[] visited;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -41,91 +45,69 @@ public class MountainRoutes {
             routes[horz][vert] = 1;
         }
 
-        System.out.println(depthFirstSearch(n, a, b, d, routes));
+        visited = new int[n];
+        depthFirstSearch(n, a, b, d, routes);
+        System.out.println(counter);
     }
 
-    public static int depthFirstSearch(int n, int a, int b, int d, int[][] routes) {
-
-        /*for(int i = 0; i < n; i++) {
-            System.out.println(Arrays.toString(routes[i]));
-        }*/
-
-        Stack routesStack = new Stack(100);
-        Stack daysStack = new Stack(100);
-        routesStack.push(a);
-
-        int numberOfDays = 0;
-        int counter = 0;
-        daysStack.push(numberOfDays);
-        int[] visited = new int[n];
-        while (!routesStack.isEmpty()) {
-
-            //routesStack.display();
-            int currentHostel = routesStack.pop();
-            //System.out.println("currentHostel = " + currentHostel);
-            numberOfDays = daysStack.pop();
-            int stackSize = routesStack.getSize();
-            //System.out.println("numberOfDays = " + numberOfDays);
-            if(numberOfDays <= d) {
-                if (currentHostel == b) {
-                    counter++;
-                    //visited = new int[n];
-                    //System.out.println("counter = " + counter);
-                } else {
-                    for (int i = 0; i < n; i++) {
-                        if (routes[currentHostel - 1][i] == 1 && visited[i] == 0) {
-                            routesStack.push(i + 1);
-                            daysStack.push(numberOfDays + 1);
+    public static void depthFirstSearch(int n, int currentHostel, int b, int d, int[][] routes) {
+        if (numberOfDays <= d) {
+            visited[currentHostel - 1] = 1;
+            if (currentHostel == b) {
+                counter++;
+            } else {
+                for (int i = 0; i < n; i++) {
+                    if (routes[currentHostel - 1][i] == 1) {
+                        int v = i + 1;
+                        if(visited[i] == 0){
+                            numberOfDays++;
+                            depthFirstSearch(n, v, b, d, routes);
+                            numberOfDays--;
+                            visited[i] = 0;
                         }
                     }
-                    if(stackSize == routesStack.getSize()) {
-                        visited[currentHostel - 1] = 1;
-                        System.out.println(currentHostel);
-                    }
-
                 }
+                visited[currentHostel - 1] = 1;
             }
+        }
+    }
 
+    private static class Stack {
+        private int maxSize;
+        private int[] stackArray;
+        private int top;
+
+        public Stack(int s) {
+            maxSize = s;
+            stackArray = new int[maxSize];
+            top = -1;
+        }
+        public void display() {
+            for(int i = 0; i <= top; i++) {
+                System.out.print(stackArray[i] + " ");
+            }
+            System.out.println();
         }
 
-        return counter;
+        public void push(int j) {
+            stackArray[++top] = j;
+        }
+        public int pop() {
+            return stackArray[top--];
+        }
+        public int peek() {
+            return stackArray[top];
+        }
+        public boolean isEmpty() {
+            return (top == -1);
+        }
+        public boolean isFull() {
+            return (top == maxSize);
+        }
+
+        public int getSize() {
+            return top;
+        }
     }
 }
 
-class Stack {
-    private int maxSize;
-    private int[] stackArray;
-    private int top;
-
-    public Stack(int s) {
-        maxSize = s;
-        stackArray = new int[maxSize];
-        top = -1;
-    }
-    public void display() {
-        for(int i = 0; i <= top; i++) {
-            System.out.print(stackArray[i] + " ");
-        }
-        System.out.println();
-    }
-
-    public void push(int j) {
-        stackArray[++top] = j;
-    }
-    public int pop() {
-        return stackArray[top--];
-    }
-    public int peek() {
-        return stackArray[top];
-    }
-    public boolean isEmpty() {
-        return (top == -1);
-    }
-    public boolean isFull() {
-        return (top == maxSize);
-    }
-
-    public int getSize() {
-        return top;
-    }
-}
