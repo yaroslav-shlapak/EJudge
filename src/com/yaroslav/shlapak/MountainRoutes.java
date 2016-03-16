0fac3450e60b19b2070acee3fda072aa9f897ccf
yaroslav <yaroslav.shlapak@gmail.com>
2016-03-17 00:15:47 +0200

    Save

--- src/com/yaroslav/shlapak/MountainRoutes.java
+++ src/com/yaroslav/shlapak/MountainRoutes.java
@@ -51,14 +51,17 @@ public class MountainRoutes {
     }
 
     public static void depthFirstSearch(int n, int currentHostel, int b, int d, int[][] routes) {
+
         if (numberOfDays <= d) {
             visited[currentHostel - 1] = 1;
+            System.out.println(currentHostel);
             if (currentHostel == b) {
                 counter++;
             } else {
                 for (int i = 0; i < n; i++) {
                     if (routes[currentHostel - 1][i] == 1) {
                         int v = i + 1;
+
                         if(visited[i] == 0){
                             numberOfDays++;
                             depthFirstSearch(n, v, b, d, routes);
@@ -67,9 +70,41 @@ public class MountainRoutes {
                         }
                     }
                 }
-                visited[currentHostel - 1] = 1;
             }
         }
+    }
+
+    public static void depthFirstSearchNonRecursive(int n, int currentHostel, int b, int d, int[][] routes) {
+        Stack hotelStack = new Stack(1000);
+        Stack daysStack = new Stack(1000);
+        hotelStack.push(currentHostel);
+        daysStack.push(numberOfDays);
+
+        while (!hotelStack.isEmpty()) {
+            currentHostel = hotelStack.pop();
+            numberOfDays = daysStack.pop();
+
+            if (numberOfDays <= d) {
+                visited[currentHostel - 1] = 1;
+                if (currentHostel == b) {
+                    counter++;
+                } else {
+                    for (int i = 0; i < n; i++) {
+                        if (routes[currentHostel - 1][i] == 1) {
+                            int v = i + 1;
+                            if (visited[i] == 0) {
+                                numberOfDays++;
+                                hotelStack.push(v);
+                                daysStack.push(numberOfDays);
+                                numberOfDays--;
+                                visited[i] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
     }
 
     private static class Stack {
@@ -95,16 +130,16 @@ public class MountainRoutes {
         public int pop() {
             return stackArray[top--];
         }
-        public int peek() {
-            return stackArray[top];
-        }
         public boolean isEmpty() {
             return (top == -1);
+        }
+
+        public int peek() {
+            return stackArray[top];
         }
         public boolean isFull() {
             return (top == maxSize);
         }
-
         public int getSize() {
             return top;
         }
