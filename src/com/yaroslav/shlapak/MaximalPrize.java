c347c52d111dcd1a8122a462cc57613535898a88
yaroslav <yaroslav.shlapak@gmail.com>
2016-03-18 02:08:40 +0200

    Save

--- src/com/yaroslav/shlapak/MaximalPrize.java
+++ src/com/yaroslav/shlapak/MaximalPrize.java
@@ -2,42 +2,82 @@ package com.yaroslav.shlapak;
 
 import java.util.Arrays;
 import java.util.Scanner;
-
+// 32888 2
 /**
  * Created by yarl on 17.03.16.
  */
+
+
 public class MaximalPrize {
+    static int max = 0;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String n = scanner.next();
         int k = scanner.nextInt();
 
         int len = n.length();
+
+
+
+
         int[] data = new int[len];
         for(int i = 0; i < len; i++) {
             data[i] = Integer.parseInt(n.substring(i, i + 1));
         }
 
-        //System.out.println(Arrays.toString(data));
+
+        if(k > len) {
+            k = len;
+        }
+        int[] indices = new int[len];
+        int[] dat = new int[len];
+        int count = 0;
 
         for(int i = 0; i < k; i++) {
+
+
             int[] d = getMax(data, i);
             if(d[0] > data[i]) {
                 swap(data, i, d[1]);
+                indices[count] = d[1];
+                dat[count] = data[d[1]];
+                count++;
             }
         }
+
+
+        for(int i = 0; i < count; i++) {
+            int[] d = getMax2(dat, count);
+            System.out.println(d[0] + " " + dat[i]);
+            if(d[0] > dat[i]) {
+                swap(data, indices[i], indices[d[1]]);
+            }
+        }
+
+
         for(int i = 0; i < len; i++) {
             System.out.print(data[i]);
         }
         System.out.println();
-
     }
+    public static int getNumber(int[] n) {
+        //System.out.println(n.toString());
+        int ret = 0;
+        for(int i = 0; i < n.length; i++) {
+            ret += n[n.length - i - 1] * pow(10, i);
+        }
+        return ret;
+    }
+
+
 
     public static void swap(int[] data, int from, int to) {
         int temp = data[from];
         data[from] = data[to];
         data[to] = temp;
     }
+
+
     public static int pow(int x, int y) {
         int power = 1;
         for(int i = 0; i < y; i++) {
@@ -50,7 +90,19 @@ public class MaximalPrize {
         int max = 0;
         int index = 0;
         for(int i = start; i < data.length; i++) {
-            if(data[i] > max) {
+            if(data[i] >= max) {
+                max = data[i];
+                index = i;
+            }
+        }
+        return new int[]{max, index};
+    }
+
+    public static int[] getMax2(int[] data, int len) {
+        int max = 0;
+        int index = 0;
+        for(int i = 0; i < len; i++) {
+            if(data[i] >= max) {
                 max = data[i];
                 index = i;
             }
